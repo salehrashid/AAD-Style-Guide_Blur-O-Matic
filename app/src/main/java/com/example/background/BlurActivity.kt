73c6bbe3d0dd987063bfccf73d4e4cccacc82716
saleh rashid <saleh.rashid.b@gmail.com>
2022-07-22 14:52:06 +0700

    initial commit

---
+++ app/src/main/java/com/example/background/BlurActivity.kt
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.background
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.work.WorkInfo
+import com.example.background.databinding.ActivityBlurBinding
+
+class BlurActivity : AppCompatActivity() {
+
+    private val viewModel: BlurViewModel by viewModels {
+        BlurViewModel.BlurViewModelFactory(
+            application
+        )
+    }
+    private lateinit var binding: ActivityBlurBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBlurBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.outputWorkInfos.observe(this, workInfosObserver())
+
+        binding.goButton.setOnClickListener { viewModel.applyBlur(blurLevel) }
+
+        binding.seeFileButton.setOnClickListener {
+            viewModel.outputUri?.let { currentUri ->
+                val actionView = Intent(Intent.ACTION_VIEW, currentUri)
+                actionView.resolveActivity(packageManager)?.run {
+                    startActivity(actionView)
+                }
+            }
+        }
+
+        binding.cancelButton.setOnClickListener {
+            viewModel.cancelWork()
+        }
+    }
+
+    private fun workInfosObserver(): Observer<List<WorkInfo>> {
+        return Observer { listOfWorkInfo ->
+            if (listOfWorkInfo.isNullOrEmpty()) {
+                return@Observer
+            }
+            val workInfo = listOfWorkInfo[0]
+
+            if (workInfo.state.isFinished) {
+                showWorkFinished()
+
+                val outputImageUri = workInfo.outputData.getString(KEY_IMAGE_URI)
+
+                if (!outputImageUri.isNullOrEmpty()) {
+                    viewModel.setOutputUri(outputImageUri)
+                    binding.seeFileButton.visibility = View.VISIBLE
+                }
+            } else {
+                showWorkInProgress()
+            }
+        }
+    }
+    private fun showWorkInProgress() {
+        with(binding) {
+            progressBar.visibility = View.VISIBLE
+            cancelButton.visibility = View.VISIBLE
+            goButton.visibility = View.GONE
+            seeFileButton.visibility = View.GONE
+        }
+    }
+
+    private fun showWorkFinished() {
+        with(binding) {
+            progressBar.visibility = View.GONE
+            cancelButton.visibility = View.GONE
+            goButton.visibility = View.VISIBLE
+        }
+    }
+
+    private val blurLevel: Int
+        get() =
+            when (binding.radioBlurGroup.checkedRadioButtonId) {
+                R.id.radio_blur_lv_1 -> 1
+                R.id.radio_blur_lv_2 -> 2
+                R.id.radio_blur_lv_3 -> 3
+                else -> 1
+            }
+}
---
+++ app/src/main/java/com/example/background/workers/SaveImageToFileWorker.kt
@@ -0,0 +1,51 @@
+package com.example.background.workers
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.provider.MediaStore
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import com.example.background.KEY_IMAGE_URI
+import com.example.background.TAG_OUTPUT
+import java.lang.Exception
+import java.util.*
+
+private const val TAG = "SaveImageToFileWorker"
+
+class SaveImageToFileWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+
+    private val title = "Blurred Image"
+    private val dateFormatter = java.text.SimpleDateFormat(
+        "yyyy.MM.dd 'at' HH:mm:ss z",
+        Locale.getDefault()
+    )
+
+    override fun doWork(): Result {
+        makeStatusNotification("Saving image", applicationContext)
+        sleep()
+
+        val resolver = applicationContext.contentResolver
+        return try {
+            val resourceUri = inputData.getString(KEY_IMAGE_URI)
+            val bitmap = BitmapFactory.decodeStream(
+                resolver.openInputStream(Uri.parse(resourceUri))
+            )
+            val imageUri = MediaStore.Images.Media.insertImage(
+                resolver, bitmap, title, dateFormatter.format(Date())
+            )
+            if (!imageUri.isNullOrEmpty()) {
+                val output = workDataOf(KEY_IMAGE_URI to imageUri)
+                Result.success(output)
+            } else {
+                Log.e(TAG, "Writing to MediaStore failed")
+                Result.failure()
+            }
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+            Result.failure()
+        }
+    }
+}
